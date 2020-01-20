64f5224d5a0e8424504b86862704c519b0cb6b2a
fdiazgilg <fdiazgilg@gmail.com>
2020-01-21 00:32:24 +0100

    Refactorizar clase Game

--- main.py
+++ main.py
@@ -1,69 +1,157 @@
 from pygame import *
 from pygame.locals import *
 import sys
+from random import randint
 
 init()
+FPS = 60
 
-class Robot:
-    speed = 2
-    images = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
+class Bomb:
+    pictures = ['bomb_01.png', 'bomb_02.png', 'bomb_03.png', 'bomb_04.png']
 
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-        self.image = image.load('resources/robot_r01.png').convert_alpha()
 
-    def go_up(self):
-        self.y = max(0, self.y - self.speed)
+        self.frames = []
+        for pict in self.pictures:
+            frame = image.load('resources/{}'.format(pict)).convert_alpha()
+            self.frames.append(frame)
+        #self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
 
-    def go_down(self):
-        self.y = min(600, self.y + self.speed)
+        self.frame_act = 0
+        self.num_frames = len(self.frames)
 
-    def go_right(self):
-        self.x = min(800, self.x + self.speed)
-    
-    def go_left(self):
-        self.x = max(0, self.x - self.speed)
-    
+        self.current_time = 0
+        self.animation_time = FPS//4
+        
+    def update(self, dt):
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.frame_act += 1
+            if self.frame_act == self.num_frames:
+                self.frame_act = 0
+            #self.frame_act = (self.frame_act + 1) % self.num_frames
+
+    @property
     def position(self):
         return self.x, self.y
     
+    @property
+    def image(self):
+        return self.frames[self.frame_act]
 
-screen = display.set_mode((800, 600))
-display.set_caption('Hola mundo!')
-background_color = (150, 150, 222)
 
-robot = Robot(400, 300)
+class Robot:
+    speed = 5
+    pictures = ['robot_r01.png', 'robot_r02.png', 'robot_r03.png', 'robot_r04.png']
 
-while True:
-    for ev in event.get():
-        if ev.type == QUIT:
-            quit()
-            sys.exit()
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
-        if ev.type == KEYDOWN:
-            if ev.key == K_UP:
-                robot.go_up()
-            if ev.key == K_DOWN:
-                robot.go_down()
-            if ev.key == K_LEFT:
-                robot.go_left()
-            if ev.key == K_RIGHT:
-                robot.go_right()
+        self.frames = []
+        for pict in self.pictures:
+            frame = image.load('resources/{}'.format(pict)).convert_alpha()
+            self.frames.append(frame)
+        #self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
 
-    keys_pressed = key.get_pressed()
-    if keys_pressed[K_UP]:
-        robot.go_up()
-    if keys_pressed[K_DOWN]:
-        robot.go_down()
-    if keys_pressed[K_LEFT]:
-        robot.go_left()
-    if keys_pressed[K_RIGHT]:
-        robot.go_right()
+        self.frame_act = 0
+        self.num_frames = len(self.frames)
 
-    print(robot.x, robot.y)
+    def change_frame(self):
+        self.frame_act += 1
+        if self.frame_act == self.num_frames:
+            self.frame_act = 0
+        
+        #self.frame_act = (self.frame_act + 1) % self.num_frames
 
-    screen.fill(background_color)
-    screen.blit(robot.image, robot.position())
 
-    display.flip()
+    def go_up(self):
+        self.y = max(0, self.y - self.speed)
+        self.change_frame()
+
+    def go_down(self):
+        self.y = min(600, self.y + self.speed)
+        self.change_frame()
+
+    def go_right(self):
+        self.x = min(800, self.x + self.speed)
+        self.change_frame()
+    
+    def go_left(self):
+        self.x = max(0, self.x - self.speed)
+        self.change_frame()
+    
+    @property
+    def position(self):
+        return self.x, self.y
+    
+    @property
+    def image(self):
+        return self.frames[self.frame_act]
+    
+class Game:
+    clock = time.Clock()
+
+    def __init__(self):
+        self.screen = display.set_mode((800, 600))
+        display.set_caption('Hola mundo!')
+
+        self.background_color = (150, 150, 222)
+
+        self.robot = Robot(400, 300)
+
+        self.bombas = []
+
+        for i in range(5):
+            self.bomb = Bomb(randint(0, 750), randint(0, 550))
+            self.bombas.append(self.bomb)
+
+    def gameOver(self):
+        quit()
+        sys.exit()
+
+    def handleEvents(self):
+        for ev in event.get():
+            if ev.type == QUIT:
+                self.gameOver()
+
+            if ev.type == KEYDOWN:
+                if ev.key == K_UP:
+                    self.robot.go_up()
+                if ev.key == K_DOWN:
+                    self.robot.go_down()
+                if ev.key == K_LEFT:
+                    self.robot.go_left()
+                if ev.key == K_RIGHT:
+                    self.robot.go_right()
+
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP]:
+            self.robot.go_up()
+        if keys_pressed[K_DOWN]:
+            self.robot.go_down()
+        if keys_pressed[K_LEFT]:
+            self.robot.go_left()
+        if keys_pressed[K_RIGHT]:
+            self.robot.go_right()
+
+    def mainloop(self):
+        while True:
+            dt = self.clock.tick(FPS)
+
+            self.handleEvents()
+
+            self.screen.fill(self.background_color)
+            self.screen.blit(self.robot.image, self.robot.position)
+            for b in self.bombas:
+                b.update(dt)
+                self.screen.blit(b.image, b.position)
+
+            display.flip()
+
+if __name__ == '__main__':
+    game = Game()
+    game.mainloop()
